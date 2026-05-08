a7d31b631532016bbd4411c1b2c2d8cd1b264415
LuanHSL <luansilvalemes@gmail.com>
2026-05-08 13:21:47 -0300

    feat: base frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Products from './pages/Products'
+import Kitchen from './pages/Kitchen'
+import Orders from './pages/Orders'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/kitchen" element={<Kitchen />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
